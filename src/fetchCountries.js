0d062d0bd39b97006c84401430a460c21a3e4ecc
Alexandr Sorokin <sanek1678@gmail.com>
2021-12-06 19:16:22 +0200

    minor changes

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,22 +1,3 @@
-import debounce from 'lodash.debounce';
-
-const DEBOUNCE_DELAY = 300;
-const textField = document.querySelector('#search-box');
-
- 
-textField.addEventListener('input',debounce(onInput,DEBOUNCE_DELAY));
-
-function onInput(event) {
-    const field = event.target;
-    const fieldContent = field.value;
-
-
-    findCountry(fieldContent)
-    //.then(renderCountries)
-    //.catch(error => {console.log(error)})
-} 
-
-
 function findCountry(countryName) {
     
 return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
@@ -26,9 +7,4 @@ return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,cap
     
 }
    
-
-// function renderCountries(country) {
-//     const markup = countryCardTpl(country);
-//     refs.cardContainer.innerHTML = markup;
-// }
-
+export default { findCountry };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 
 import './css/styles.css';
 import '/fetchCountries';
+import debounce from 'lodash.debounce';
+import API from '/fetchCountries';
 
 
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
 
+const DEBOUNCE_DELAY = 300;
+const textField = document.querySelector('#search-box');
+
+ 
+textField.addEventListener('input',debounce(onInput,DEBOUNCE_DELAY));
+
+function onInput(event) {
+    const field = event.target;
+    const fieldContent = field.value;
 
 
+    API.findCountry(fieldContent)
+    //.then(renderCountries)
+    //.catch(error => {console.log(error)}).finaly(() => field.reset())
+} 
+
+
+
+// function renderCountries(country) {
+//     const markup = countryCardTpl(country);
+//     refs.cardContainer.innerHTML = markup;
+// }
