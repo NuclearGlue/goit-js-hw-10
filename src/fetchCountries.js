74a36f1c3eceb40b1339f5b8b235cde25cd1dc57
Alexandr Sorokin <sanek1678@gmail.com>
2021-12-06 18:32:01 +0200

    fetch created

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,34 @@
-let countryChoose = 'Ukraine'
-fetch(`https://restcountries.com/v3.1/name/${countryChoose}?fields={name.official},{capital},{population},{flags.svg},{languages}`)
-    .then(response => {
-        if (!response.ok) {
-      throw new Error(response.status);
-    }
-        return response.json()
-        
-    }).then(country => {
-        console.log(country);
-        const flag = countryCardTpl(country);
-    }).catch(error => {
-    console.log(error)
-})
+import debounce from 'lodash.debounce';
+
+const DEBOUNCE_DELAY = 300;
+const textField = document.querySelector('#search-box');
+
+ 
+textField.addEventListener('input',debounce(onInput,DEBOUNCE_DELAY));
+
+function onInput(event) {
+    const field = event.target;
+    const fieldContent = field.value;
+
+
+    findCountry(fieldContent)
+    //.then(renderCountries)
+    //.catch(error => {console.log(error)})
+} 
+
+
+function findCountry(countryName) {
+    
+return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
+     .then(response => {
+        return response.json();
+    })
+    
+}
+   
+
+// function renderCountries(country) {
+//     const markup = countryCardTpl(country);
+//     refs.cardContainer.innerHTML = markup;
+// }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+
 import './css/styles.css';
 import '/fetchCountries';
-import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
 
-const textField = document.querySelector('#search-box')
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+
+
+
